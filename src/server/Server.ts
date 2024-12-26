66b6d1517952d0e87649fd6cce06b035557756ca
vinisilveir4 <viniciussilverk@gmail.com>
2024-12-26 20:02:49 -0300

    add tradução de mensagens yup

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config" // disponibiliza as variaveis ambiente pra aplicação
 
+import "./shared/services/TranslationsYup"; // importa arquivo de configurações de mensagem do yup
 import { router } from "./routes";
 
 const server = express(); // cria instancia do express
